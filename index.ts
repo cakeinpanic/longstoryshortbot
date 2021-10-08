72330a23b3fb1b8612fc5e76773b12ba0812c01a
cakeinpanic <cakeinpanic@gmail.com>
2021-10-08 23:24:23 +0300

    remove one hook

--- index.ts
+++ index.ts
@@ -11,7 +11,6 @@ const token = process.env.TOKEN
 const bot = new TelegramBot(token, { polling: true })
 const messages$ = new Subject<TelegramMessage>()
 
-bot.on('channel_post', gotMessage)
 bot.on('message', gotMessage)
 
 function gotMessage(msg) {
