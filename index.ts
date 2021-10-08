f3105a50ff59a1ee54eb3c3aeff51c1d2d3fb7b0
cakeinpanic <cakeinpanic@gmail.com>
2021-10-08 11:27:32 +0300

    add merge mechanism

--- index.ts
+++ index.ts
@@ -1,8 +1,7 @@
 import { config } from 'dotenv'
 import TelegramBot from 'node-telegram-bot-api'
-import { groupBy, mergeMap, Subject } from 'rxjs'
+import { groupBy, map, mergeMap, Subject } from 'rxjs'
 import { groupMessagesCameTogether } from './handleMessages'
-
 import { TelegramMessage } from './interface'
 
 config()
@@ -10,19 +9,24 @@ config()
 const token = process.env.TOKEN
 
 const bot = new TelegramBot(token, { polling: true })
-
-const subj = new Subject<TelegramMessage>()
+const messages$ = new Subject<TelegramMessage>()
 
 bot.on('channel_post', gotMessage)
 bot.on('message', gotMessage)
 
 function gotMessage(msg) {
-  const chatId = msg.chat.id
-  subj.next(msg)
-
+  messages$.next(msg)
   // send a message to the chat acknowledging receipt of their message
   //bot.sendMessage(chatId, 'Received your message');
 }
 
-subj.pipe(groupBy<TelegramMessage, number>(({ chat }) => chat.id), mergeMap(t => groupMessagesCameTogether(t))).subscribe(console.log)
+messages$.pipe(
+  groupBy(({ chat }) => chat.id),
+  mergeMap((messageStreamPerUser$) => {
+    return groupMessagesCameTogether(messageStreamPerUser$)
+      .pipe(map((messages: TelegramMessage[]) => ({ messages, chatId: messageStreamPerUser$.key })))
+  })
+).subscribe(({ messages, chatId }: { messages: TelegramMessage[], chatId: number }) => {
+  bot.sendMessage(chatId, messages.join('\n'));
+})
 
