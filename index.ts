16ae974510a057ddb3a08fb2af668d47a7803c37
cakeinpanic <cakeinpanic@gmail.com>
2024-02-22 15:31:30 +0100

    add express and logginig

--- index.ts
+++ index.ts
@@ -1,25 +1,42 @@
-import { config } from 'dotenv'
-import TelegramBot from 'node-telegram-bot-api'
-import { Subject } from 'rxjs'
-import { CollectedMessages, handleMessagesStreamByUser } from './handleMessages'
-import { TelegramMessage } from './interface'
+import { config } from "dotenv";
+import TelegramBot from "node-telegram-bot-api";
+import { Subject } from "rxjs";
+import {
+  CollectedMessages,
+  handleMessagesStreamByUser,
+} from "./handleMessages";
+import { TelegramMessage } from "./interface";
 
-config()
+config();
 
-const token = process.env.TOKEN
+const token = process.env.TOKEN;
 
-const bot = new TelegramBot(token, { polling: true })
-const messages$ = new Subject<TelegramMessage>()
+const bot = new TelegramBot(token, { polling: true });
+const messages$ = new Subject<TelegramMessage>();
 
-bot.on('message', gotMessage)
+bot.on("message", gotMessage);
 
 function gotMessage(msg) {
-  messages$.next(msg)
+  console.log(JSON.stringify(msg, null, 2))
+  messages$.next(msg);
 }
 
-handleMessagesStreamByUser(messages$, 1000)
-  .subscribe(({ messages, chatId, error }: CollectedMessages) => {
-    messages && bot.sendMessage(chatId, messages.join('\n'))
-    error && bot.sendMessage(chatId, error)
-  })
+handleMessagesStreamByUser(messages$, 1000).subscribe(
+  ({ messages, chatId, error }: CollectedMessages) => {
+    messages && bot.sendMessage(chatId, messages.join("\n"));
+    error && bot.sendMessage(chatId, error);
+  }
+);
 
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  console.log("Got a ping");
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
